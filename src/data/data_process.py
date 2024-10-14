d5942cfa96b250db405cfd527bd500a11b243204
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-10-14 18:29:33 -0400

    added suppurt to ibis backend consummer
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- src/data/data_process.py
+++ src/data/data_process.py
@@ -1,11 +1,12 @@
 from sqlmodel import create_engine
 from sqlalchemy.exc import OperationalError
-from ..dao.consumer_table import create_consumer_table, select_all_consumers
+from ..dao.consumer_table import create_consumer_table
 from ..dao.economic_indicators_table import create_indicators_table, select_all_indicators
 from .data_pull import DataPull
 from datetime import datetime
 import polars.selectors as cs
 import polars as pl
+import ibis
 import os
 
 class DataProcess(DataPull):
@@ -40,6 +41,10 @@ class DataProcess(DataPull):
             os.makedirs(f'{data_dir}/raw')
         if not os.path.exists(f'{data_dir}/processed'):
             os.makedirs(f'{data_dir}/processed')
+        if self.database_url.startswith("sqlite"):
+            self.conn = ibis.sqlite.connect(self.database_url.replace("sqlite:///", ""))
+        else:
+            raise Exception("Database url is not supported")
 
     def consumer_data(self, update:bool=False) -> pl.DataFrame:
         """
@@ -55,16 +60,11 @@ class DataProcess(DataPull):
         -------
         pl.DataFrame
         """
-        try:
-            df = pl.DataFrame(select_all_consumers(self.engine))
-            if df.is_empty() or update:
-                return self.process_consumer(update)
-            else:
-                return df
-        except OperationalError:
-            return self.process_consumer(update)
+        if "consumertable" not in self.conn.list_tables() or update:
+            self.process_consumer(update)
+        return self.conn.table("consumertable").to_polars()
 
-    def process_consumer(self, update:bool=False) -> pl.DataFrame:
+    def process_consumer(self, update:bool=False) -> None:
         """
         Processes the consumer data and stores it in the database. If the data does 
         not exist, it will pull the data from the source.
@@ -78,46 +78,38 @@ class DataProcess(DataPull):
         -------
         pl.DataFrame
         """
-        if not os.path.exists(f"{self.data_dir}/raw/consumer.xls") or update:
-            self.pull_consumer(f"{self.data_dir}/raw/consumer.xls")
-        try:
-            return pl.DataFrame(select_all_consumers(self.engine))
-        except OperationalError:
-            create_consumer_table(self.engine)
-            df = pl.read_excel(f"{self.data_dir}/raw/consumer.xls", sheet_id=1)
-            names = df.head(1).to_dicts().pop()
-            names = {k: self.clean_name(v) for k, v in names.items()}
-            df = df.rename(names)
-            df = df.tail(-2).head(-1)
-            df = df.with_columns(pl.col('descripcion').str.to_lowercase())
-            df = df.with_columns((
-                pl.when(pl.col('descripcion').str.contains("ene")).then(pl.col('descripcion').str.replace("ene", "01").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("feb")).then(pl.col('descripcion').str.replace("feb", "02").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("mar")).then(pl.col('descripcion').str.replace("mar", "03").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("abr")).then(pl.col('descripcion').str.replace("abr", "04").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("may")).then(pl.col('descripcion').str.replace("may", "05").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("jun")).then(pl.col('descripcion').str.replace("jun", "06").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("jul")).then(pl.col('descripcion').str.replace("jul", "07").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("ago")).then(pl.col('descripcion').str.replace("ago", "08").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("sep")).then(pl.col('descripcion').str.replace("sep", "09").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("oct")).then(pl.col('descripcion').str.replace("oct", "10").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("nov")).then(pl.col('descripcion').str.replace("nov", "11").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .when(pl.col('descripcion').str.contains("dic")).then(pl.col('descripcion').str.replace("dic", "12").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
-                  .otherwise(pl.col('descripcion').str.split_exact('-', 1).struct.rename_fields(['year', 'month']).alias('date'))
-              )).unnest("date")
-            df = df.with_columns((
-              pl.when((pl.col("year").str.len_chars() == 2) & (pl.col("year").str.strip_chars().cast(pl.Int32) < 80)).then(pl.col("year").str.strip_chars().cast(pl.Int32) + 2000)
-                .when((pl.col("year").str.len_chars() == 2) & (pl.col("year").str.strip_chars().cast(pl.Int32) >= 80)).then(pl.col("year").str.strip_chars().cast(pl.Int32) + 1900)
-                .otherwise(pl.col("year").str.strip_chars().cast(pl.Int32)).alias("year")
-            ))
-            df = df.with_columns(date=pl.date(pl.col('year').cast(pl.String), pl.col('month'), 1)).sort(by="date")
-            df = df.with_columns(pl.col("date").cast(pl.String))
-            df = df.drop(['year', 'month', 'descripcion'])
-            df = df.with_columns(pl.all().exclude("date").cast(pl.Float64))
-            df = df.with_columns(id=pl.col("date").rank().cast(pl.Int64))
-            df.write_parquet(f"{self.data_dir}/processed/consumer.parquet")
-            df.write_database(table_name="consumertable", connection=self.database_url, if_table_exists="replace")
-            return pl.DataFrame(select_all_consumers(self.engine))
+        create_consumer_table(self.engine)
+        df = pl.read_excel(f"{self.data_dir}/raw/consumer.xls", sheet_id=1)
+        names = df.head(1).to_dicts().pop()
+        names = {k: self.clean_name(v) for k, v in names.items()}
+        df = df.rename(names)
+        df = df.tail(-2).head(-1)
+        df = df.with_columns(pl.col('descripcion').str.to_lowercase())
+        df = df.with_columns((
+            pl.when(pl.col('descripcion').str.contains("ene")).then(pl.col('descripcion').str.replace("ene", "01").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("feb")).then(pl.col('descripcion').str.replace("feb", "02").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("mar")).then(pl.col('descripcion').str.replace("mar", "03").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("abr")).then(pl.col('descripcion').str.replace("abr", "04").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("may")).then(pl.col('descripcion').str.replace("may", "05").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("jun")).then(pl.col('descripcion').str.replace("jun", "06").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("jul")).then(pl.col('descripcion').str.replace("jul", "07").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("ago")).then(pl.col('descripcion').str.replace("ago", "08").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("sep")).then(pl.col('descripcion').str.replace("sep", "09").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("oct")).then(pl.col('descripcion').str.replace("oct", "10").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("nov")).then(pl.col('descripcion').str.replace("nov", "11").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .when(pl.col('descripcion').str.contains("dic")).then(pl.col('descripcion').str.replace("dic", "12").str.split_exact('-', 1).struct.rename_fields(['month', 'year']).alias('date'))
+            .otherwise(pl.col('descripcion').str.split_exact('-', 1).struct.rename_fields(['year', 'month']).alias('date'))
+        )).unnest("date")
+        df = df.with_columns((
+        pl.when((pl.col("year").str.len_chars() == 2) & (pl.col("year").str.strip_chars().cast(pl.Int32) < 80)).then(pl.col("year").str.strip_chars().cast(pl.Int32) + 2000)
+            .when((pl.col("year").str.len_chars() == 2) & (pl.col("year").str.strip_chars().cast(pl.Int32) >= 80)).then(pl.col("year").str.strip_chars().cast(pl.Int32) + 1900)
+            .otherwise(pl.col("year").str.strip_chars().cast(pl.Int32)).alias("year")
+        ))
+        df = df.with_columns(date=pl.date(pl.col('year').cast(pl.String), pl.col('month'), 1)).sort(by="date")
+        df = df.with_columns(pl.col("date").cast(pl.String))
+        df = df.drop(['year', 'month', 'descripcion'])
+        df = df.with_columns(pl.all().exclude("date").cast(pl.Float64))
+        self.conn.insert("consumertable", df)
 
     def clean_name(self, name:str) -> str:
         """
