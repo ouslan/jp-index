f65b03ae247471231118d235b090e80f7c657c3c
sebastian-dejesus <sebastian.dejesus3@upr.edu>
2024-10-26 16:37:14 -0400

    changed program in main to class with methods #12

--- src/data/monte_carlo.py
+++ src/data/monte_carlo.py
@@ -2,39 +2,50 @@ import pandas as pd
 from scipy.stats import truncnorm
 from tqdm import tqdm
 
+class monteCarlo():
+    def __init__(self, simulation_count):
+        self.simulation_count = simulation_count
+
+    def simulate(self, years, year_constants, upper, lower, std_dev):
+        sim_values = pd.DataFrame()
+        years = pd.Series(years)
+        counter = 0
+        for i in year_constants:
+            values_list = []
+            for _ in tqdm(range(self.simulation_count)):
+                sim_num = truncnorm.rvs((lower - i)/std_dev, (upper - i)/std_dev, loc=i, scale=std_dev, size=1)
+                values_list.append(sim_num.item())
+            sim_values[f"{years[counter]}"] = pd.Series(values_list)
+            print(f"Year {years[counter]} done")
+            counter += 1
+        return sim_values
+    
+    def percentiles(self, sim_results):
+        all_percentiles = pd.DataFrame()
+        for column in sim_results:
+            perc_1 = float(sim_results[f"{column}"].quantile(0.01))
+            perc_3 = float(sim_results[f"{column}"].quantile(0.0275))
+            perc_5 = float(sim_results[f"{column}"].quantile(0.05))
+            perc_50 = float(sim_results[f"{column}"].quantile(0.5))
+            perc_95 = float(sim_results[f"{column}"].quantile(0.95))
+            perc_97 = float(sim_results[f"{column}"].quantile(0.975))
+            perc_99 = float(sim_results[f"{column}"].quantile(0.99))
+            percentiles = pd.Series([perc_1, perc_3, perc_5, perc_50, perc_95, perc_97, perc_99])
+            all_percentiles[f"{column}"] = percentiles
+        return all_percentiles.T
+
 def main():
     # Male year constants simulation
     std_dev_m = 0.2996471
     std_dev_f = 0.3225237
     upper_normal = .8
     lower_normal = -0.3236444
-    simulation_n = 100000
 
 
-    df = pd.read_csv("kt_p.csv")
-    df2 = pd.DataFrame()
-    df3 = pd.DataFrame()
-    count = 0
-    for i in df["kt_m_p"]:
-        sim_list = []
-        for _ in tqdm(range(simulation_n)):
-            n = truncnorm.rvs((lower_normal - i)/std_dev_m, (upper_normal - i)/std_dev_m, loc=i, scale=std_dev_m, size=1)
-            sim_list.append(n.item())
-        df2[f"{df["year"].iloc[count]}"] = pd.Series(sim_list)
-        print(f"Year {df["year"].iloc[count]} done")
-        count += 1
-    for column in df2:
-        perc_1 = float(df2[f"{column}"].quantile(0.01))
-        perc_3 = float(df2[f"{column}"].quantile(0.0275))
-        perc_5 = float(df2[f"{column}"].quantile(0.05))
-        perc_50 = float(df2[f"{column}"].quantile(0.5))
-        perc_95 = float(df2[f"{column}"].quantile(0.95))
-        perc_97 = float(df2[f"{column}"].quantile(0.975))
-        perc_99 = float(df2[f"{column}"].quantile(0.99))
-        percentiles = pd.Series([perc_1, perc_3, perc_5, perc_50, perc_95, perc_97, perc_99])
-        df3[f"{column}"] = percentiles
-    df3.T.to_csv("mc_simulation_output.csv")
-
+    male_constants_p = pd.read_csv("kt_p.csv")
+    mc = monteCarlo(1000)
+    mc.percentiles(mc.simulate(male_constants_p["year"], male_constants_p["kt_m_p"], upper_normal, lower_normal, std_dev_m)).to_csv("kt_sim.csv")
+    
 
 if __name__ == "__main__":
     main()
