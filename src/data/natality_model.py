556732d49faf4c673f7c08f74c01d495a1cb5621
spicyguac19 <sebastian.dejesus3@upr.edu>
2024-11-14 13:01:42 -0400

    added covar matrix, eigenvalue and eigenvector methods #12

--- src/data/natality_model.py
+++ src/data/natality_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy import linalg
 
 
 class natModel():
@@ -18,7 +19,7 @@ class natModel():
         return averages
    
 
-    def centralized_matrix(self):
+    def centralized_frame(self):
         # Create empty matrix with the years from the logged death rates matrix as the index
         centered_matrix = pd.DataFrame(index = self.tfr.index)
         tfr_t = self.tfr.copy().T.reset_index(drop=True).T
@@ -27,13 +28,29 @@ class natModel():
             centered_matrix[i] = pd.concat([pd.Series(tfr_t[i] - self.nat_constants()[i])], axis=1)
         return centered_matrix
 
+    def nat_covar(self):
+        cent_matrix = self.centralized_frame().to_numpy()
+        cent_matrix_T = self.centralized_frame().T.to_numpy()
+        covar_matrix = np.matmul(cent_matrix_T, cent_matrix)
+        return covar_matrix
+    
+    def eigenvals(self):
+        covar_matrix = self.nat_covar()
+        w, v = linalg.eigh(covar_matrix)
+        return pd.Series(w)
+    
+    def eigenvect(self):
+        covar_matrix = self.nat_covar()
+        w, v = linalg.eigh(covar_matrix)
+        return pd.DataFrame(v)
 
 
 def main():
     nat_data = pd.read_csv("tfr_data.csv").set_index("year")
     nat_model = natModel(nat_data)
-    print(nat_model.centralized_matrix())
+    print(nat_model.eigenvals())
+    print(nat_model.eigenvect())
 
-
+    
 if __name__ == "__main__":
     main()
